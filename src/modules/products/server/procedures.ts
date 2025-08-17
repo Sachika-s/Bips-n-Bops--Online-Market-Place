bf0f4c35b6645161615481413117aa9c36237dfc
Sachika-s <Saxens20@mcmaster.ca>
2025-08-18 03:37:52 +0400

    Feature-added product list UI

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -4,12 +4,16 @@ import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { Sort, Where } from "payload";
 import z from "zod";
 import { sortValue } from "../search-params";
+import { Media } from "@/payload-types";
+import { DEFAULT_LIMIT } from "@/constants";
 
 
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure
     .input(
         z.object({
+            cursor: z.number().default(1),
+            limit: z.number().default(DEFAULT_LIMIT),
             category:z.string().nullable().optional(),
             minPrice :z.string().nullable().optional(),
              maxPrice :z.string().nullable().optional(),
@@ -99,6 +103,12 @@ export const productsRouter = createTRPCRouter({
         });
 
     
-        return data;
+        return {
+            ...data,
+            docs: data.docs.map((doc) => ({
+                ...doc,
+                image: doc.image as Media | null,
+            }))
+        };
     }),
 }); 
